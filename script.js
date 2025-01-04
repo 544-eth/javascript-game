e1735a195e2c18219d490f41655e0b8a39616d5a
544-eth <elioku20@gmail.com>
2025-01-04 09:32:38 +0100

    initial commit

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+const collisionCanvas = document.getElementById('collisionCanvas')
+const collisionCtx = collisionCanvas.getContext('2d')
+collisionCanvas.width = window.innerWidth
+collisionCanvas.height = window.innerHeight
+
+let score = 0
+ctx.font = '30px Impact'
 
 let timeToNexRaven = 0
 let ravenInterval = 500
@@ -12,7 +19,7 @@ class Raven {
     constructor(){
         this.spriteWidth = 271
         this.spriteHeight = 194
-        this.sizeModifier = Math.random() * 0.6 + 0.4
+        this.sizeModifier = Math.random() * 0.5 + 0.3
         this.width = this.spriteWidth * this.sizeModifier
         this.height = this.spriteHeight * this.sizeModifier
         this.x = canvas.width
@@ -24,19 +31,40 @@ class Raven {
         this.image.src = 'ravens.png'
         this.frame = 0
         this.maxFrame = 4
-        
+        this.timeSinceFlap = 0
+        this.flapInterval = Math.random() * 50 + 50
+        this.randomColors = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)]
     }
-    update(){
+    update(deltaTime){
+        if (this.y < 0 || this.y > canvas.height - this.height){
+            this.directionY = this.directionY * -1
+        }
         this.x -= this.directionX
+        this.y += this.directionY
         if (this.x < 0 - this.width) this.markedForDeletion = true
-        if (this.frame > this.maxFrame) this.frame = 0
-        else this.frame++
+        this.timeSinceFlap += deltaTime;
+        if (this.timeSinceFlap > this.flapInterval){
+            if (this.frame > this.maxFrame) this.frame = 0
+            else this.frame++
+            this.timeSinceFlap  = 0
+        }
     }
     draw(){
         ctx.strokeRect(this.x, this.y, this.width, this.height)
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
 }
+
+
+function drawScore(){
+    ctx.fillStyle = 'black'
+    ctx.fillText('Score: ' + score, 50, 75)
+}
+
+window.addEventListener('click', function(e){
+    const detectPixelColor = ctx.getImageData(e.x, e.y, 1, 1)
+    console.log(detectPixelColor)
+})
 
 function animate(timeStamp){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -47,6 +75,7 @@ function animate(timeStamp){
         ravens.push(new Raven())
         timeToNexRaven = 0
     }
+    drawScore();
     [...ravens].forEach(object => object.update(deltaTime));
     [...ravens].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion)
